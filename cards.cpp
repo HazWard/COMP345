799e07a065edc859d52901d6c96fee658bc5a2c3
emilio <emilio@emilios-mbp.wireless.concordia.ca>
2017-09-14 17:24:03 -0400

    building skeleton for card file and adding TODOs

---
+++ cards.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#include <cards.h>
+
+class deck {
+    /*
+    */
+    public:
+        void;
+    private:
+        void;
+}
+
+deck::deck(void){
+    //TODO: Initialize the deck object based on the game rules
+}
+
+class hand {
+    /*
+    */
+}
+
+hand::hand(void){
+    //TODO: Initialize hand object that tracks list of cards
+}
+
+class card {
+    public:
+        char type;
+    private:
+        void;
+}
+
+card::card(void){
+    //TODO: Initialize the object and determine its type
+}
+
+int main(){
+    return 0;
+}
