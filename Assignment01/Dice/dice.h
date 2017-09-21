f1392b3b9a9bd29c76f4de9c762f44bec8682434
Hagop Awakian <hagopavakian@hotmail.com>
2017-09-21 15:45:27 -0400

    Created a branch and updated source

--- Assignment01/Dice/dice.h
+++ Assignment01/Dice/dice.h
@@ -4,10 +4,20 @@
 class Dice
 {
 private:
-	int rollNum;
+	int numRolls;
 	int numTracker[6];
 	int numOfDice;
+	int numOfArmies;
 public:
 	Dice();
-
+	void numOfArmies();
+	void numGenerator();
+	int getNumRolls();
+	int getNumTracker();
+	int getNumOfDice();
+	int getNumOfArmies();
+	void setNumRolls(int numRolls);
+	void setNumTracker(int index);
+	void setNumOfDice(int numOfDice);
+	void setNumOfArmies(int numOfArmies);
 };
