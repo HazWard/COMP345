78e7bfae56b1dc5f5da76cd7140f41498e52f373
emilio <emilio@emilios-mbp.wireless.concordia.ca>
2017-09-21 10:08:40 -0400

    Part 5 done: Can finish driver and move on to other parts.

--- cards.cpp
+++ cards.cpp
@@ -29,15 +29,16 @@ Card::Card(int t, Card *nextCard){
 
 std::ostream& operator<< (std::ostream &strm, const Card &card) {
     switch(card.type) {
-        case INFANTRY : return strm << "Infantry Card"; break;
-        case CAVALRY : return strm << "Cavalry Card"; break;
-        case ARTILLERY : return strm << "Artillery Card"; break;
+        case INFANTRY : return strm << "Infantry Card";
+        case CAVALRY : return strm << "Cavalry Card";
+        case ARTILLERY : return strm << "Artillery Card";
         default : return strm << "Card Error";
     }
 }
 
 class Deck{
     /*
+    Implemented as a LinkedList but could also be done as a Vector (C++ equivalent of Java's ArrayList)
     */
     int numberOfCards;
     protected:
@@ -45,7 +46,7 @@ class Deck{
     public:
         Deck(int numberOfCountries);
         ~Deck(void);
-        int draw(void);
+        Card* draw(void);
         void display(void);
 };
 
@@ -73,6 +74,7 @@ Deck::Deck(int numberOfCountries){
             //int r = rand() % static_cast<int>(3); //non distributed random generator
             if(cardType[r] > 0){
                 top = new Card(r, top);
+                cardType[r]--;
                 break;
             }
         }
@@ -89,12 +91,11 @@ Deck::~Deck(void){
     temp = NULL;
 }
 
-int Deck::draw(void){
+Card* Deck::draw(void){
     Card *temp = top;
     top = top->next;
     int out = temp->type;
-    delete temp;
-    return out;
+    return temp;
 }
 
 void Deck::display(void){
@@ -114,18 +115,57 @@ class Hand{
     int inf, cav, art;
     public:
         bool exchange(int t);
-        void addCard(Card c);
+        void draw(Card *c);
+        void display(void);
 };
 
 bool Hand::exchange(int t){
-    return true;
+    switch(t) {
+        case INFANTRY : 
+            if(inf >= 3){
+                inf = inf - 3;
+                return true;
+            }
+            return false;
+        case CAVALRY : 
+            if(cav >= 3){
+                cav = cav - 3;
+                return true;
+            }
+            return false;
+        case ARTILLERY :
+            if(art >= 3){
+                art = art - 3;
+                return true;
+            }
+            return false;
+        default : return false;
+    }
 }
 
-void Hand::addCard(Card c){
+void Hand::draw(Card *c){
+    switch(c->type) {
+        case INFANTRY : inf++; break;
+        case CAVALRY : cav++; break;
+        case ARTILLERY : art++; break;
+        default : break;
+    }
+}
+
+void Hand::display(void){
+    cout << "This hand contains " << inf << " infantry cards, " << cav << " cavalry cards, and "
+    << art << " artillery cards." << endl;
 }
 
 int main(){
-    Deck *d = new Deck(12);
+    Deck *d = new Deck(9);
     d->display();
+    Hand *h = new Hand();
+    h->draw(d->draw());
+    h->display();
+    h->draw(d->draw());
+    h->draw(d->draw());
+    h->display();
+    cout << (h->exchange(INFANTRY) == 1 ? "True" : "False") << " " << (h->exchange(INFANTRY) == 1 ? "True" : "False") << endl;
     return 0;
 }
