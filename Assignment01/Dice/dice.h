d87ab98f5bfbdabc3c5918c8c0937ad84128c193
Hagop Awakian <hagopavakian@hotmail.com>
2017-09-23 14:59:34 -0400

    Dice is done
    Only test cases are left

--- Assignment01/Dice/dice.h
+++ Assignment01/Dice/dice.h
@@ -1,23 +1,13 @@
-
+#include <vector>
+using namespace std;
 
 
 class Dice
 {
 private:
-	int numRolls;
 	int numTracker[6];
-	int numOfDice;
-	int numOfArmies;
 public:
-	Dice();
-	void numOfArmies();
-	void numGenerator();
-	int getNumRolls();
-	int getNumTracker();
-	int getNumOfDice();
-	int getNumOfArmies();
-	void setNumRolls(int numRolls);
-	void setNumTracker(int index);
-	void setNumOfDice(int numOfDice);
-	void setNumOfArmies(int numOfArmies);
+	int numGenerator();
+	vector<int> Dice::howManyDice(int howManyDice);
+	int* getNumTracker();
 };
